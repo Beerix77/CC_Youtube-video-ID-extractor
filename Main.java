03f62ebb6d5f4fb2dcf5d618da180a78cd798a34
Andrew <andrewmatysiak@hotmail.com>
2022-10-07 20:24:32 +1100

    commit3 Work in progress

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -7,9 +9,10 @@ public class Main {
         Scanner keyboard = new Scanner(System.in);
         //String url = keyboard.nextLine();
 
-        String test = "https://www.youtube.com/watch?v=RRW2aUSw5vU";
+        String test = "https://youtu.be/RRW2aUSw5vU";
+        String test2 = "https://www.youtube.com/watch?v=kbxkq_w51PM";
 
-        String[] splitUrl = test.split("");
+        String[] splitUrl = test2.split("");
         System.out.println(Arrays.toString(splitUrl));
 
         int index = 0;
@@ -18,14 +21,36 @@ public class Main {
             if (splitUrl[i].equals("/")){
                 slashCount++;
             }
+            index = i;
         }
-        System.out.println(slashCount);
+
+        System.out.println("Index of final slash: " + index);
+
+        // START HERE BELOW:
+        // Regex for 'youtu.be' pattern, ID is located immediately following 3rd slash
+
+        //boolean shortURL = false;
+        Pattern dotPattern = Pattern.compile("youtu.be");
+        Matcher searchURL = dotPattern.matcher(test2);
+        boolean matchFound = searchURL.find();
+        if(matchFound) {
+            System.out.println("Match found");
+            //shortURL = true;
+            for (int i = ++index; i <= splitUrl.length-1; i++){
+                System.out.print(splitUrl[i]);
+            }
 
 
+        } else {
+            System.out.println("Match not found");
+            System.out.println(test2.indexOf("="));
 
-
-
-
-
+        }
     }
 }
+/* redo:
+-do test for '=' in array
+if yes then do index of '='
+do test for 'youtu.be'
+
+ */
