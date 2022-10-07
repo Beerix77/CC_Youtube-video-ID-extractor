b0e407ace283ad32cf95a13c220ec3b532ba5426
Andrew <andrewmatysiak@hotmail.com>
2022-10-07 19:40:26 +1100

    commit2 Work un progress

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         //String url = keyboard.nextLine();
 
         String test = "https://www.youtube.com/watch?v=RRW2aUSw5vU";
+
+        String[] splitUrl = test.split("");
+        System.out.println(Arrays.toString(splitUrl));
+
+        int index = 0;
+        int slashCount = 0;
+        for (int i = 0; slashCount < 3; i++){
+            if (splitUrl[i].equals("/")){
+                slashCount++;
+            }
+        }
+        System.out.println(slashCount);
+
 
 
 
@@ -14,7 +28,4 @@ public class Main {
 
 
     }
-
-
-
 }
