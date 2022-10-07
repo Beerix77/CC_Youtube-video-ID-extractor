9523020e7b31efaa818bd760b4fb07352ae17514
Andrew <andrewmatysiak@hotmail.com>
2022-10-07 19:22:34 +1100

    initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        //String url = keyboard.nextLine();
+
+        String test = "https://www.youtube.com/watch?v=RRW2aUSw5vU";
+
+
+
+
+
+
+    }
+
+
+
+}
